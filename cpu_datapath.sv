f10049d92d0b8513890d29e7bebfe2d88a71aa8b
Nathan Thomason <thomasn2@illinois.edu>
2015-10-23 20:40:19 -0500

    adding changes to cpu datapath

--- cpu_datapath.sv
+++ cpu_datapath.sv
@@ -5,4 +5,44 @@ module cpu_datapath
 		input clk
 );
 
+
+
+instruction_fetch IF_Logic
+();
+
+latch_if_id IF_ID_Latch
+(
+		.clk(clk)
+);
+
+
+instruction_decode ID_Logic
+();
+
+latch_id_ex ID_EX_Latch
+(
+		.clk(clk)
+);
+
+
+execution_module EX_module
+();
+
+latch_ex_mem EX_MEM_Latch
+(
+		.clk(clk)
+);
+
+
+memory_module Mem_Module
+();
+
+latch_wb WB_latch
+(
+		.clk(clk)
+);
+
+writeback_module WB_Module
+();
+
 endmodule : cpu_datapath
