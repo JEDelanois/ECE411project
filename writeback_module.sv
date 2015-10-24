26f1ea7be54e8b999b6a8aa8ceb6b039fd5e33d4
JEDelanois <jedelanois@gmail.com>
2015-10-24 16:17:47 -0500

    fixed compile errors in writeback module

--- writeback_module.sv
+++ writeback_module.sv
@@ -1,5 +1,8 @@
+import lc3b_types::*;
+
 module writeback_module
 (
+	input clk,
 	input [15:0] currALU,
 	input [15:0] MDR,
 	input [15:0] currIR,
@@ -7,10 +10,10 @@ module writeback_module
 	input [15:0] lc3b_control,
 	input [15:0] genCC_WB,
 	
-	output logic [15:0] currALUout
-	output logic [15:0] MDRout
+	output logic [15:0] currALUout,
+	output logic [15:0] MDRout,
 	output logic [15:0] br_adder_out,
-	output logic branch_enable;
+	output logic branch_enable
 
 );
 
@@ -18,23 +21,23 @@ module writeback_module
 assign currALUout = currALU;
 assign MDRout = MDR;
 
-logic lc3b_word adj9out;
-logic lc3b_word adj11out;
-logic lc3b_word adjMUXout;
+ lc3b_word adj9out;
+ lc3b_word adj11out;
+ lc3b_word adjMUXout;
 logic [2:0] ccin,ccout;
 
 //calculate branch offset
- adj (parameter = 9) adj9
+ adj #(.width(9)) adj9
 (	
-	in (currIR[8:0]),
-   out (adj9out)   
+	.in(currIR[8:0]),
+   .out (adj9out)   
 );
 
 
- adj (parameter = 11) adj11
+ adj #(.width(11)) adj11
 (	
-	in (currIR[10:0]),
-   out (adj11out)   
+	.in(currIR[10:0]),
+   .out(adj11out)   
 );
 
 
@@ -42,19 +45,19 @@ logic [2:0] ccin,ccout;
 mux2 adjMUX
 (
 	/* port declaration */
-	sel(),
-	a(adj9out), 
-	b(adj11out),
-	f(adjMUXout)
+	.sel(),
+	.a(adj9out), 
+	.b(adj11out),
+	.f(adjMUXout)
 );
 
 
 adder branchAdder
 (
 	/* port declarations */
-	a(adjMUXout), 
-	b(currPC),
-	sum(br_adder_out)
+	.a(adjMUXout), 
+	.b(currPC),
+	.sum(br_adder_out)
 );
 
 
@@ -76,9 +79,9 @@ register #(.width(3)) cc
     .out(ccout)
 );
 
-CCCOMP cccomp
+cccomp CCcomp
 (
-	.dest(currIR[11:9]), 
+	.testcc(currIR[11:9]), 
 	.cc(ccout),
 	.branch_enable(branch_enable)
 );
