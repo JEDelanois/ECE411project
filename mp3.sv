8d1b8f8ae8b84123b128ab0566d6047affc935e8
jpandoh <pandoh2@illinois.edu>
2015-10-25 01:00:14 -0500

    set mem byte enable

--- mp3.sv
+++ mp3.sv
@@ -15,13 +15,18 @@ module mp3
 		/* Memory signals*/
 		output lc3b_word mem_addr1,
 		output logic mem_read1,
+		output logic mem_write1,
 		input lc3b_word mem_rdata1,
 		output lc3b_word mem_addr2,
 		output logic mem_read2,
 		output logic mem_write2,
+		output lc3b_mem_wmask mem_byte_enable2,
 		input lc3b_word mem_rdata2,
 		output lc3b_word mem_wdata2
 );
+
+assign mem_write1 = 0;
+assign mem_byte_enable2 = 2'b11;
 
 /*
 cache LC3b_Cache
--- mp3_tb.sv
+++ mp3_tb.sv
@@ -5,8 +5,10 @@ timeprecision 1ns;
 
 logic clk;
 logic mem_read1;
+logic mem_write1;
 logic mem_read2;
 logic mem_write2;
+logic [1:0] mem_byte_enable2;
 logic [15:0] mem_addr1;
 logic [15:0] mem_addr2;
 logic [15:0] mem_rdata1;
@@ -24,10 +26,12 @@ mp3 dut
     /* Memory signals*/
     .mem_addr1,
     .mem_read1,
+    .mem_write1,
     .mem_rdata1,
     .mem_addr2,
     .mem_read2,
     .mem_write2,
+    .mem_byte_enable2,
     .mem_rdata2,
     .mem_wdata2
 );
@@ -38,7 +42,7 @@ magic_memory_dp magic_memory_dp
 
     /* Port A */
     .read_a(mem_read1),
-    .write_a(1'b0),
+    .write_a(mem_write1),
     .wmask_a(),
     .address_a(mem_addr1),
     .wdata_a(),
@@ -48,7 +52,7 @@ magic_memory_dp magic_memory_dp
     /* Port B */
     .read_b(mem_read2),
     .write_b(mem_write2),
-    .wmask_b(),
+    .wmask_b(mem_byte_enable2),
     .address_b(mem_addr2),
     .wdata_b(mem_wdata2),
     .resp_b(),
